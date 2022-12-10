fb3120741462e6fd53c088b42935f46d4f06a463
Veron Ibishi <veron@gjirafa.com>
2022-12-11 00:17:15 +0100

    Master Commit

--- src/models/index.js
+++ src/models/index.js
@@ -1,32 +1,9 @@
+import UserModel from "./user-model"
+import ThrowFirebaseErrors from "./throw-firebase-errors"
+import ThrowAuthErrors from "./throw-auth-errors"
 
-class UserModel { 
-        constructor(fullName,email,password) {
-                
-                this.fullName = fullName
-                this.email = email
-                this.password = password
-        }
+export {
+    UserModel,
+    ThrowFirebaseErrors,
+    ThrowAuthErrors
 }
-
-class ThrowError { 
-
-        constructor(error) { 
-            this.error = error
-        }
-    
-        message() { 
-            switch (this.error.code) { 
-                case 'auth/email-already-in-use': { 
-                    return  { field: 'email', message: 'This email already exists'}
-                }
-                case 'auth/invalid-email': { 
-                    return { field: 'email', message: 'This is an invalid email'}
-                }
-                case 'auth/weak-password': {
-                    return { field: 'password', message: 'Password should be at least 6 characters'}
-                }
-            }
-        }
-}
-
-export { UserModel, ThrowError }
---
+++ src/models/user-model.js
@@ -0,0 +1,8 @@
+export default class UserModel { 
+    constructor(fullName,email,password) {
+            
+            this.fullName = fullName
+            this.email = email
+            this.password = password
+    }
+}
