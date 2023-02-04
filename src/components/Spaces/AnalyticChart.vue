94d5a761f523320eb06214b289b2f1d54fcd2f46
Veron Ibishi <veron@gjirafa.com>
2023-02-04 22:42:41 +0100

    Analytics

---
+++ src/components/Spaces/AnalyticChart.vue
@@ -0,0 +1,61 @@
+<script setup>
+import { Chart, Line } from 'vue-chartjs'
+import { options } from '../../models/chart'
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Filler,
+  Title,
+  Tooltip,
+  Legend
+} from 'chart.js'
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  Filler,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+)
+
+ChartJS.defaults.backgroundColor = 'f4f4fc'
+ChartJS.defaults.color = 'black'
+ChartJS.defaults.borderColor = 'f4f4fc'
+
+
+defineProps({
+  collection: {
+    type: Object,
+    required: false,
+    default: () => {}
+  }
+})
+
+const datas =  {
+  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+  datasets: [
+    {
+      label: 'Data One',
+      color: '#60a5fa',
+      backgroundColor: '#60a5fa',
+      data: [40, 39, 10, 40, 39, 80, 40]
+    }
+  ]
+}
+</script>
+
+<template>
+  <div class="flex flex-row items-stretch">
+    <Line
+      class="h-full"
+      :data="collection"
+      :options="options"
+    />
+  </div>
+</template>
--- src/components/Spaces/OrdersListing.vue
+++ src/components/Spaces/OrdersListing.vue
@@ -19,7 +19,6 @@ onMounted(() => {
 
 const handleScroll = (evt) => {
   let element = scrollList.value
-  console.log(element, element.getBoundingClientRect().bottom, window.innerHeight + 40)
     if(element.getBoundingClientRect().bottom < window.innerHeight + 40) { 
        emits('load-more')
     }
@@ -34,14 +33,14 @@ const transformDates = (date) => {
   <ol
     v-if="orders"
     ref="scrollList"
-    class="border-2 bg-zinc-50 shadow-sm m-4 rounded"
+    class=" bg-zinc-50 flex flex-col items-center shadow-sm m-4 rounded"
   >
     <li
       v-for="order in orders"
       :key="order._id"
-      class="bg-white flex flex-row justify-between shadow-md mb-2 p-2"
+      class="bg-white w-fit sm:w-full flex flex-col items-center md:flex-row justify-between shadow-md mb-2 p-2"
     >
-      <div class="flex flex-col ml-2">
+      <div class="flex flex-col ml-2 w-full md:w-auto ">
         <span class="text-zinc-500">Ordered By</span>
         <div class="flex flex-row items-center">
           <div class="w-8 h-8 bg-zinc-900 rounded-full" />
@@ -55,28 +54,34 @@ const transformDates = (date) => {
           </div>
         </div>
       </div>
-      <div class="flex flex-col">
+      <div class="flex flex-col ml-2 w-full md:w-auto">
         <span class="text-zinc-500">Order</span>
         <div class="lowercase text-sm">
-          <p class="mb-1"><span class="lowercase font-bold">Started:</span>{{ transformDates(order['order-duration'].startAt) }}</p>
+          <p class="mb-1">
+            <span class="lowercase font-bold">Started:</span>{{ transformDates(order['order-duration'].startAt) }}
+          </p>
           <p><span class="lowercase font-bold">Ends:</span> {{ transformDates(order['order-duration'].endAt) }}</p>
         </div>
       </div>
-      <div class="flex flex-col items-center justify-between">
-        <span class="flex flex-col">Status</span>
-        <div :class="` mb-2 rounded-lg text-white text-sm p-1 ${order['order-duration'].completed ? 'bg-green-400' : 'bg-blue-400'}`">{{ order['order-duration'].completed ? 'Completed' : 'Ongoing' }}</div>
-      </div>
-      <div class="flex flex-col items-center justify-between">
-        <span>Type</span>
-        <div class="rounded-lg mb-2 text-white bg-zinc-900 p-1">{{ order['order-privilege'] }}</div>
-      </div>
-      <div class="flex flex-col">
-        <span>Price</span>
-        <small class="m-auto bg-green-400 rounded-lg w-8 text-center">{{ order['order-price'] }}€</small>
-      </div>
-      <div class="flex flex-col">
+      <div class="flex flex-col md:items-center justify-between ml-2 w-full md:w-auto">
         <span>Ordered At</span>
         <p>{{ transformDates(order['ordered-at']) }}</p>
+      </div>
+      <div class="flex flex-col md:items-center justify-between ml-2 w-full md:w-auto">
+        <span class="flex flex-col">Status</span>
+        <div :class="`w-fit md:w-auto mb-2 rounded-lg text-white text-sm p-1 ${order['order-duration'].completed ? 'bg-green-400' : 'bg-blue-400'}`">
+          {{ order['order-duration'].completed ? 'Completed' : 'Ongoing' }}
+        </div>
+      </div>
+      <div class="flex flex-col md:items-center justify-between ml-2 w-full md:w-auto">
+        <span>Type</span>
+        <div class="w-fit md:w-auto rounded-lg mb-2 text-white bg-zinc-900 p-1">
+          {{ order['order-privilege'] }}
+        </div>
+      </div>
+      <div class="flex flex-col md:items-center justify-between ml-2 w-full md:w-auto">
+        <span>Price</span>
+        <small class="w-fit bg-green-400 md:w-auto rounded-lg mb-2 text-white p-1">{{ order['order-price'] }}€</small>
       </div>
     </li>
   </ol>
