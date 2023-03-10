c443a15a488961e703f05c88c6d831b7b926213c
Veron Ibishi <veronibishijb@gmail.com>
2023-03-10 20:01:09 +0100

    fix

--- src/components/MapSideList.vue
+++ src/components/MapSideList.vue
@@ -1,8 +1,8 @@
 <script setup>
 import { Map } from 'mapbox-gl'
-import { GoogleRoute } from "@/models/map";
+import { googleRoute } from "@/helpers/map";
 
-defineProps({
+const props = defineProps({
   locations: {
     type: Array,
     required: true,
@@ -12,30 +12,105 @@ defineProps({
     required: true
   }
 })
-const returnGoogleLink = (lng,lat) => GoogleRoute(lng,lat)
+const returnGoogleLink = (lng,lat) => googleRoute(lng,lat)
+
+const moveMap = (location) => {
+  props.map.flyTo({
+    center: [location.location.lng,location?.location.lat],
+    zoom: 11,
+  })
+}
 </script>
 
 <template>
-  <div class="flex flex-col overflow-y-auto h-full w-3/12 bg-slate-50">
-    <div
-      v-for="location in locations"
-      :key="location"
-      class="border-2 border-red-500 p-2"
-    >
-      <h3>{{ location.title }}</h3>
-      <div>
+  <div class="flex flex-col overflow-y-auto h-full w-4/12 bg-slate-50">
+    <h1 class="text-center mt-3 mb-3">
+      Available Parking Locations
+    </h1>
+    <div class="flex flex-col overflow-y-scroll h-10/12">
+      <div
+        v-for="location in locations"
+        :key="location"
+        @click="moveMap(location)"
+        class="mb-4 border-2 border-slate-100 rounded-lg mr-2 flex flex-col   shadow-sm hover:shadow-md hover:shadow-blue-200"
+      >
         <div class="flex flex-col">
-          <small> Parking Space:{{ location.amount }}</small>
-          <small>Location: {{ location.location.locationName }}</small>
-          <div class="flex flex-row">
-            <button class="border-2 border-red-500 w-fit p-2">Order</button>
-            <button class="border-2 border-blue-400">Save</button>
+          <div
+            id="parking-info__header"
+            class="flex flex-row justify-between border-b border-blue-300 items-center"
+          >
+            <h1 class="text-slate-700 m-2 font-sans tracking-wide text-xl uppercase">
+              {{ location.title }}
+            </h1>
+            <span class="text-slate-600 m-2 flex flex-row">
+              <img
+                src="../assets/parking-icon.png"
+                class="w-4 h-4 mr-2"
+                alt=""
+              >
+              <small class="font-bold">{{ location.amount }}</small>
+            </span>
           </div>
-          <a class="border-2 border-red-500 w-fit w-6 h-6 object-cover" :href="returnGoogleLink(location.location.lng,location.location.lat)" target="_blank">
-            <img class="w-4 h-4" src="../assets/map/google.svg" alt="">
-          </a>
+          <div class="flex flex-col m-2 justify-start">
+            <h2 class="text-slate-600 mb-2">
+              Country: <strong>{{ location.location.country }}</strong>
+            </h2>
+            <h3 class="text-slate-600 mb-2">
+              Address: <strong>{{ location.location.locationName }}</strong>
+            </h3>
+            <h4 class="text-slate-600 mb-2">
+              Parking Spaces: <strong>{{ location.amount }}</strong>
+            </h4>
+          </div>
+          <div class="flex flex-row justify-between p-2">
+            <button @click="redirectMe(location)" class="rounded-lg border border-blue-400 text-blue-400 px-2 hover:border-blue-600 hover:text-blue-600">
+              Order
+            </button>
+            <div class="mr-4">
+              <a
+                class="w-fit w-6 h-6 object-cover"
+                :href="returnGoogleLink(location.location.lng,location.location.lat)"
+                target="_blank"
+              >
+                <img
+                  class="w-4 h-4"
+                  src="../assets/map/google.svg"
+                  alt=""
+                >
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
   </div>
 </template>
+
+<style>
+/* Set the width and height of the scrollbar */
+::-webkit-scrollbar {
+  width: 6px;
+  height: 6px;
+}
+
+/* Track styles */
+::-webkit-scrollbar-track {
+  background-color: #E2E8F0;
+}
+
+/* Handle styles */
+::-webkit-scrollbar-thumb {
+  background-color: #4A5568;
+  border-radius: 3px;
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background-color: #718096;
+}
+
+/* Handle when active (i.e. being dragged) */
+::-webkit-scrollbar-thumb:active {
+  background-color: #1A202C;
+}
+</style>
